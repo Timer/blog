97324f20fa3041d01f982999b80255f24e310125
Joe Haddad <timer150@gmail.com>
2019-12-07 17:00:59 -0500

    Add a main page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import format from 'date-fns/format';
+import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
 import { BlogPost, getPosts } from '../utils/posts';
@@ -13,6 +14,9 @@ export default function Home({ posts }: { posts: BlogPost[] }) {
     <main className="page-content" aria-label="Content">
       <div className="wrapper">
         <div className="home">
+          <Head>
+            <title>timer.blog</title>
+          </Head>
           <h1 className="page-heading">timer.blog</h1>
           <h2 className="post-list-heading">Posts</h2>
           <ul className="post-list">
